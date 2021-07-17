aed6a7f18eb9ff99d9fdcc7978b54c08aa4ba93e
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-07-17 17:41:33 -0400

    Rename array variables to more meaningful names

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const path = require('path');
 
 let srcDirPath = '../1\ Foundational\ Years/';
 
-let srcBaseArr = fs.readdirSync(srcDirPath);
+let srcFileNameArr = fs.readdirSync(srcDirPath);
 
 let targetYear = new Date().getFullYear().toString();
 let targetMonth = new Date().getMonth().toString();
 if (targetMonth < 10) {
     targetMonth = '0'.concat(targetMonth);
 }
-let relativePathString = `/wp-content/uploads/${targetYear}/${targetMonth}/`;
+let wordpressUploadsDirString = `/wp-content/uploads/${targetYear}/${targetMonth}/`;
 
 let srcFullArr = [];
-srcBaseArr.forEach(src => {
-    srcFullArr.push(`${relativePathString}${src}`);
+srcFileNameArr.forEach(src => {
+    srcFullArr.push(`${wordpressUploadsDirString}${src}`);
 });
 console.log(srcFullArr);
