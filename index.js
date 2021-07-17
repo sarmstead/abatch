d496b77776e92e55116b6560b76150e94573b3f0
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-07-17 18:28:14 -0400

    Create anchorContentFilePath variable

--- index.js
+++ index.js
@@ -3,32 +3,45 @@ const path = require('path');
 
 let srcDirPath = '../1\ Foundational\ Years/';
 
-let srcFileNameArr = fs.readdirSync(srcDirPath);
+const createSrc = (srcDirPath) => {
+    let srcFileArr = fs.readdirSync(srcDirPath);
+    let targetYear = new Date().getFullYear().toString();
+    let targetMonth = new Date().getMonth().toString();
+    if (targetMonth < 10) {
+        targetMonth = '0'.concat(targetMonth);
+    }
+    let wordpressUploadsDirString = `/wp-content/uploads/${targetYear}/${targetMonth}/`;
 
-let targetYear = new Date().getFullYear().toString();
-let targetMonth = new Date().getMonth().toString();
-if (targetMonth < 10) {
-    targetMonth = '0'.concat(targetMonth);
+    let srcFullArr = [];
+    srcFileArr.forEach(src => {
+        // Replace spaces with proper UTF-8 character
+        newSrc = src.split(' ').join('%20');
+
+        srcFullArr.push(`${wordpressUploadsDirString}${newSrc}`);
+    });
+
+    return srcFullArr;
 }
-let wordpressUploadsDirString = `/wp-content/uploads/${targetYear}/${targetMonth}/`;
-
-let srcFullArr = [];
-srcFileNameArr.forEach(src => {
-    // Replace spaces with proper UTF-8 character
-    newSrc = src.split(' ').join('%20');
-
-    srcFullArr.push(`${wordpressUploadsDirString}${newSrc}`);
-});
-
-let anchorContentFilePath = '../anchor-content/foundational.txt';
 
 try {
-    const fileContents = fs.readFileSync(anchorContentFilePath, 'utf-8');
-    let anchorContent = fileContents.split('\n');
-    console.log(anchorContent);
-  } catch (err) {
+    createSrc(srcDirPath);
+    console.log(srcDirPath);
+} catch (err) {
     console.error(err);
 }
+
+
+
+
+// let anchorContentFilePath = '../anchor-content/foundational.txt';
+
+// try {
+//     const fileContents = fs.readFileSync(anchorContentFilePath, 'utf-8');
+//     let anchorContent = fileContents.split('\n');
+//     console.log(anchorContent);
+//   } catch (err) {
+//     console.error(err);
+// }
 
 
 // console.log(srcFullArr);
