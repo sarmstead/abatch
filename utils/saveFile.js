e40251d92193f4709b67b21c56f4eab6b6c6800f
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-07-17 22:06:36 -0400

    Move saveFile function into seperate module

---
+++ utils/saveFile.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+const saveFile = (anchorArray, fileName) => {
+    // Create an 'anchors' directory if it doesn't exist
+    const targetDirectory = path.join(__dirname, '../', 'anchors');
+    if(!fs.existsSync(targetDirectory)) {
+        fs.mkdirSync(targetDirectory);
+    }
+
+    // Create a file with the contents of the anchorArray
+    anchorArray.forEach(anchor => {
+        fs.open(path.join(__dirname, '../anchors', `${fileName}.txt`), 'a', 666, (event, id) => {
+            fs.write(id, anchor + os.EOL, null, 'utf-8', () => {
+                fs.close(id, () => {
+                    return;
+                })
+            })
+        })
+    })
+    
+}
+
+module.exports = saveFile;
