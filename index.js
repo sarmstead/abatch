904533eb2afd0eb1e68a85e0afec89d15ff2d099
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-07-18 19:30:12 -0400

    Add src directory requiredOption to program

--- index.js
+++ index.js
@@ -9,22 +9,29 @@ const saveFile = require('./utils/saveFile');
 
 program.version('0.1.1').description('Simple anchor link generator for WordPress');
 
-// User prompts
-let srcDirPath = prompt('What is your src directory path? ');
-let anchorContentFilePath = prompt('What is your anchor content file path? ');
-let fileName = prompt('What file name do you want to save to? ');
+program
+    .requiredOption('-s, --src <directory-path>', 'Set local directory path for PDFs')
+    .parse();
 
-try {
-    let anchorSrc = createSrc(srcDirPath);
-    let anchorContent = createContent(anchorContentFilePath);
-    let fullAnchor = [];
-    if(anchorSrc.length !== anchorContent.length) {
-        console.error(chalk.red('The files provided do not have the same number of entries'));
-    }
-    for (i=0; i < anchorSrc.length; i++) {
-        fullAnchor.push(`<a href="${anchorSrc[i]}" target="_blank">${anchorContent[i]}</a>`);
-    }
-    saveFile(fullAnchor, fileName);
-  } catch (err) {
-    console.error(err);
-}
+// User prompts
+let options = program.opts();
+let srcDirPath = options.src;
+// let anchorContentFilePath = prompt('What is your anchor content file path? ');
+// let fileName = prompt('What file name do you want to save to? ');
+
+// try {
+//     let anchorSrc = createSrc(program.opts({src}));
+//     let anchorContent = createContent(anchorContentFilePath);
+//     let fullAnchor = [];
+//     if(anchorSrc.length !== anchorContent.length) {
+//         console.error(chalk.red('The files provided do not have the same number of entries'));
+//     }
+//     for (i=0; i < anchorSrc.length; i++) {
+//         fullAnchor.push(`<a href="${anchorSrc[i]}" target="_blank">${anchorContent[i]}</a>`);
+//     }
+//     saveFile(fullAnchor, fileName);
+//   } catch (err) {
+//     console.error(err);
+// }
+
+console.log(program.opts().src);
