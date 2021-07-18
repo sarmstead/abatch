fe7a28f8081cca323b3baffe9221b84dc0e079cd
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-07-17 21:59:25 -0400

    Move createSrc function into seperate module

---
+++ utils/createSrc.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const createSrc = (srcDirPath) => {
+    let srcFileArr = fs.readdirSync(srcDirPath);
+    let targetYear = new Date().getFullYear().toString();
+    let targetMonth = new Date().getMonth().toString();
+    if (targetMonth < 10) {
+        targetMonth = '0'.concat(targetMonth);
+    }
+    let wordpressUploadsDirString = `/wp-content/uploads/${targetYear}/${targetMonth}/`;
+
+    let srcFullArr = [];
+    srcFileArr.forEach(src => {
+        // Replace spaces with proper UTF-8 character
+        newSrc = src.split(' ').join('%20');
+
+        srcFullArr.push(`${wordpressUploadsDirString}${newSrc}`);
+    });
+
+    return srcFullArr;
+}
+
+module.exports = createSrc;
