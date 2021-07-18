915c968b7a21769e1aa36feb98fa357d76b75054
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-07-17 22:16:43 -0400

    Update directory path

--- utils/saveFile.js
+++ utils/saveFile.js
@@ -4,14 +4,14 @@ const os = require('os');
 
 const saveFile = (anchorArray, fileName) => {
     // Create an 'anchors' directory if it doesn't exist
-    const targetDirectory = path.join(__dirname, '../', 'anchors');
+    const targetDirectory = path.join(__dirname, '../../', 'anchors');
     if(!fs.existsSync(targetDirectory)) {
         fs.mkdirSync(targetDirectory);
     }
 
     // Create a file with the contents of the anchorArray
     anchorArray.forEach(anchor => {
-        fs.open(path.join(__dirname, '../anchors', `${fileName}.txt`), 'a', 666, (event, id) => {
+        fs.open(path.join(__dirname, '../../anchors', `${fileName}.txt`), 'a', 666, (event, id) => {
             fs.write(id, anchor + os.EOL, null, 'utf-8', () => {
                 fs.close(id, () => {
                     return;
