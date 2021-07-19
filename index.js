63983d3b3c56db4dc8272bcea639ae9e7ede6017
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-07-18 20:34:22 -0400

    Revert  to commit id 470bf68

--- index.js
+++ index.js
@@ -1,50 +1,27 @@
 const prompt = require('prompt-sync')({sigint: true});
 const chalk = require('chalk');
-const { program } = require('commander');
 
 // Import modules
 const createSrc = require('./utils/createSrc');
 const createContent = require('./utils/createContent');
 const saveFile = require('./utils/saveFile');
 
-program.version('0.1.1').description('Simple anchor link generator for WordPress');
-
-program
-    .command('src <srcPath>')
-    .description('Set local directory path for PDFs')
-    .action(function(path) {
-        console.log(path);
-    });
-    
-
-program
-    .command('cnt <contentPath>')
-    .description('Set anchor content file path')
-    .action(function(path) {
-        console.log(path);
-    });    
-    
-program.parse();
-
 // User prompts
-// let options = program.opts();
-// let srcDirPath = options.src;
-// let anchorContentFilePath = options.cnt;
-// let fileName = prompt('What file name do you want to save to? ');
+let srcDirPath = prompt('What is your src directory path? ');
+let anchorContentFilePath = prompt('What is your anchor content file path? ');
+let fileName = prompt('What file name do you want to save to? ');
 
-// try {
-//     let anchorSrc = createSrc(program.opts({src}));
-//     let anchorContent = createContent(anchorContentFilePath);
-//     let fullAnchor = [];
-//     if(anchorSrc.length !== anchorContent.length) {
-//         console.error(chalk.red('The files provided do not have the same number of entries'));
-//     }
-//     for (i=0; i < anchorSrc.length; i++) {
-//         fullAnchor.push(`<a href="${anchorSrc[i]}" target="_blank">${anchorContent[i]}</a>`);
-//     }
-//     saveFile(fullAnchor, fileName);
-//   } catch (err) {
-//     console.error(err);
-// }
-
-// console.log(program.opts());
+try {
+    let anchorSrc = createSrc(srcDirPath);
+    let anchorContent = createContent(anchorContentFilePath);
+    let fullAnchor = [];
+    if(anchorSrc.length !== anchorContent.length) {
+        console.error(chalk.red('The files provided do not have the same number of entries'));
+    }
+    for (i=0; i < anchorSrc.length; i++) {
+        fullAnchor.push(`<a href="${anchorSrc[i]}" target="_blank">${anchorContent[i]}</a>`);
+    }
+    saveFile(fullAnchor, fileName);
+  } catch (err) {
+    console.error(err);
+}
